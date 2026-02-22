781463039e90c8949b33f38c629fe1ea3930cade
Ayush Tiwari <iam.ayush886@gmail.com>
2026-02-22 22:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (84.49%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binaryGap(int n) {
+         int max = 0;
+        int pos = 0;
+        int lastPos = -1;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {
+                if (lastPos != -1) {
+                    max = Math.max(max, pos - lastPos);
+                }
+                lastPos = pos;
+            }
+            pos++;
+            n >>= 1;
+        }
+
+        return max;
+    }
+}
