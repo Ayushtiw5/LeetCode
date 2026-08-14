4919025cae149711881b6b78a766595912064e2a
Ayush Tiwari <iam.ayush886@gmail.com>
2026-08-14 22:55:37 +0530

    Time: 3 ms (72.48%) | Memory: 52.9 MB (22.11%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        int candidate1 = 0;
+        int candidate2 = 0;
         int count1 = 0;
         int count2 = 0;
-        int cand1 = Integer.MIN_VALUE;
-        int cand2 = Integer.MIN_VALUE;
         for(int i=0; i<nums.length; i++){
-            if(count1 == 0 && nums[i] != cand2){
-                cand1 = nums[i];
-                count1 = 1;
-            }else if(count2 == 0 && nums[i] != cand1 ){
-                cand2 = nums[i];
-                count2 = 1;
-            }else if(nums[i] == cand1){
+            if(nums[i] == candidate1){
                 count1++;
-            }else if(nums[i] == cand2){
+            }else if(nums[i] == candidate2){
                 count2++;
+            }else if(count1 == 0){
+                candidate1 = nums[i];
+                count1 = 1;
+            }else if(count2 == 0){
+                candidate2 = nums[i];
+                count2 = 1;
             }else{
                 count1--;
                 count2--;
             }
         }
-        List<Integer> ans = new ArrayList<>();
-         count1 = 0;
-         count2 = 0;
+        count1 = 0;
+        count2 = 0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i] == cand1){
+            if(nums[i] == candidate1){
                 count1++;
-            }else if(nums[i] == cand2){
+            }if(nums[i] == candidate2){
                 count2++;
-
             }
-
         }
-        if(count1 > nums.length / 3) ans.add(cand1);
-        if(count2 > nums.length / 3) ans.add(cand2);
+        int n = nums.length;
+        if(count1 > n/3){
+            ans.add(candidate1);
+        }
+        if(count2 > n/3 && candidate2 != candidate1){
+            ans.add(candidate2);
+        }
         return ans;
     }
 }
